761ad93724481d9549604eaa97515259b0f57e62
John Money <johnmoney@getpantheon.com>
2024-09-09 12:53:19 -0400

    PolyLang known issues: https://docs.pantheon.io/plugins-known-issues

--- web/wp-config.php
+++ web/wp-config.php
@@ -19,3 +19,6 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/config/application.php';
 
 require_once ABSPATH . 'wp-settings.php';
+
+define('PLL_COOKIE', 'STYXKEY_pll_language');
+
